3c16decd3f3a1e9686f9eb204d4c8953b5960ab3
Ali zaib <alizaib12220@gmail.com>
2024-03-14 02:02:37 +0500

    My custom post type creation

--- wp-content/plugins/first_plugin/inc/custom_post_type.php
+++ wp-content/plugins/first_plugin/inc/custom_post_type.php
@@ -4,5 +4,16 @@ add_action('init','first_plugin_news_post');
 
 function first_plugin_news_post(){
 
-    register_post_type()
+    register_post_type('news',array(
+
+        'label'=> 'first_plugin_post',
+        'labels'=> array(
+
+        ),
+        'public'=> true,
+        'description'=> 'First plugin post type creation By Ali zaib (wordpress developer)',
+         'supports'=> ['title','editor','comments','custom_fields', 'thumbnail']
+
+
+    ));
 }
