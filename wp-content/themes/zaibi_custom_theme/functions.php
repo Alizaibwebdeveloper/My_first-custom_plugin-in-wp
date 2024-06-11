322d1a7995200c89bb0e7a8eaec602ec18df733e
Ali zaib <alizaib12220@gmail.com>
2024-06-11 20:07:53 +0500

    singelton autoloader method in my theme

--- wp-content/themes/zaibi_custom_theme/functions.php
+++ wp-content/themes/zaibi_custom_theme/functions.php
@@ -1,32 +1,31 @@
 <?php
-    
-/*
+/**
+ * Main Theme functions
+ *
+ * @package Zaibi-custom-theme
+ */
 
-Main Theme function!
-@package Zaibi-custom-theme
-
-
-*/
-
-
-function zaibi_cutom_theme_enque(){
-
-wp_enqueue_style('style-css',get_stylesheet_uri() );
-wp_enqueue_script('main-js', get_template_directory_uri().'/assets/main.js',[], true);
-
-
-// Adding Boostrap in our theme
-
-    // Enqueue Bootstrap Stylesheet
-
-
-      wp_enqueue_style('bootstrap-css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css');
-
-    // Enqueue Bootstrap JS
-      wp_enqueue_script('bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js', array('jquery'), null, true);
-
+if ( ! defined( 'ZAIBI_CUSTOM_THEME' ) ) {
+    define( 'ZAIBI_CUSTOM_THEME', untrailingslashit( get_template_directory() ) );
 }
 
-add_action( 'wp_enqueue_scripts', 'zaibi_cutom_theme_enque' );
 
-?>
+
+require_once ZAIBI_CUSTOM_THEME . '/inc/helpers/autoloader.php';
+
+function zaibi_custom_theme_enqueue() {
+    // Enqueue main stylesheet
+    wp_enqueue_style( 'style-css', get_stylesheet_uri() );
+
+    // Enqueue main JavaScript
+    wp_enqueue_script( 'main-js', get_template_directory_uri() . '/assets/main.js', array(), null, true );
+
+    // Enqueue Bootstrap Stylesheet
+    wp_enqueue_style( 'bootstrap-css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css' );
+
+    // Enqueue Bootstrap JS
+    wp_enqueue_script( 'bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js', array( 'jquery' ), null, true );
+}
+
+add_action( 'wp_enqueue_scripts', 'zaibi_custom_theme_enqueue' );
+
---
+++ wp-content/themes/zaibi_custom_theme/inc/traits/trait_singleton.php
@@ -0,0 +1,29 @@
+<?php
+    
+
+    namespace Zaibi_Custom_Theme\Inc\Traits;
+
+    trait singelton{
+        public function __construct(){
+            
+
+        }
+
+        public  function __clone(){
+
+        }
+
+        final public static function get_instance(){
+
+            static $instance = [];
+            $called_class = get_called_class();
+            if(!isset($instance[$called_class])){
+
+                $instance[$called_class] = new $called_class();
+
+                do_action(sprintf('zaibi_custom_theme_singelton_init%s', $called_class));
+            }
+
+            return $instance[$called_class];
+        }
+    }
